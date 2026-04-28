a5dd47c0d30614f3864fbecedc4e1d4f40d949fe
rsovkovic <rsovkovic@gmail.com>
2026-04-28 13:02:38 +0300

    fixed cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,13 @@ import { errorHandler } from './middleware/errorHandler';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use(logger);
